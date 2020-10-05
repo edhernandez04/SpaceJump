91da4ee660b17ac63c181f6a98287323c13d88d3
edhernandez04 <mchh.004@gmail.com>
2020-10-05 14:58:50 -0400

    ship selector menu

--- App/components/ShipSelector.js
+++ App/components/ShipSelector.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import {View, Image, Text, StyleSheet} from 'react-native'
-import Images from '../assets/Images'
+import React, {useState} from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const ShipSelector = () => {
-    return (
-        <View style={styles.selectorContainer}>
-            <Image source={Images.plane3} style={{height: 50, width: 100}}/>
-            <Image source={Images.plane3} style={{height: 50, width: 100}}/>
-        </View>
-    )
-}
+const ShipSelector = (props) => {
+  return (
+    <View style={styles.selectorContainer}>
+      <TouchableOpacity onPress={() => {props.selectShip('tiny')}}>
+        <Image source={require('../assets/tiny_ship3.png')} style={{height: 50, width: 100}} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {props.selectShip('falcon')}}>
+        <Image source={require('../assets/falcon_ship3.png')} style={{height: 50, width: 100}} />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    selectorContainer: {
-        margin: 20
-    }
-})
+  selectorContainer: {
+    margin: 20,
+    flexDirection: 'row'
+  },
+});
 
-export default ShipSelector
+export default ShipSelector;
--- App/screens/Menu.js
+++ App/screens/Menu.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react';
 import { View, TextInput, Button, Text, StyleSheet, StatusBar } from 'react-native'
+import Images from '../assets/Images'
 import ShipSelector from '../components/ShipSelector'
 
 const Menu = props => {
     const [name, changeName] = useState('')
+    const [ship, selectShip] = useState('')
 
     return (
         <View style={styles.fullScreenMenu}>
             <StatusBar barStyle="light-content" />
             <Text style={styles.subHeadingText}>Select Your Ship</Text>
-            <ShipSelector />
+            <ShipSelector ship={ship} selectShip={selectShip}/>
             <TextInput value={name} onChangeText={changeName} placeholder='ENTER NAME' placeholderTextColor='white' />
             <Button title='START' onPress={() =>
                 props.navigation.navigate('Game')
