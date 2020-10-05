ed044d897ec9873e438f8f45a6c74792c86d572e
edhernandez04 <mchh.004@gmail.com>
2020-10-05 13:40:18 -0400

    selector

--- App/assets/Images.js
+++ App/assets/Images.js
@@ -1,11 +1,19 @@
+let shipName
 
+ship = selection => {
+    if (selection === 'Apollo'){
+      shipName = 'tiny'
+    } else if (selection === 'Falcon'){
+      shipName = 'falcon'
+    }
+}
 
 export default Images = {
   background: require('./Space_1.png'),
   floor: require('./floor.png'),
   hazardTop: require('./hazardTop.png'),
   hazardBody: require('./astroids.png'),
-  plane1: require('./tiny_ship1.png'),
-  plane2: require('./tiny_ship2.png'),
-  plane3: require('./tiny_ship3.png'),
+  plane1: require('./falcon_ship1.png'),
+  plane2: require('./falcon_ship2.png'),
+  plane3: require('./falcon_ship3.png'),
 };
--- App/components/ShipSelector.js
+++ App/components/ShipSelector.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import {View, Image, Text} from 'react-native'
+import {View, Image, Text, StyleSheet} from 'react-native'
 import Images from '../assets/Images'
 
 const ShipSelector = () => {
     return (
-        <View>
+        <View style={styles.selectorContainer}>
+            <Image source={Images.plane3} style={{height: 50, width: 100}}/>
             <Image source={Images.plane3} style={{height: 50, width: 100}}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    selectorContainer: {
+        margin: 20
+    }
+})
 
 export default ShipSelector
